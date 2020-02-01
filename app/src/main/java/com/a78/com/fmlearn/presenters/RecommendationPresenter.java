ca14657877afc6bdf295be8012702467c8107722
ddd78 <578579047@qq.com>
2020-02-01 16:05:23 +0800

    完成首页推荐页面滑动显示内容2

--- app/src/main/java/com/a78/com/fmlearn/presenters/RecommendationPresenter.java
+++ app/src/main/java/com/a78/com/fmlearn/presenters/RecommendationPresenter.java
@@ -38,6 +38,9 @@ public class RecommendationPresenter implements IRecommendationPresenter {
                 }
             }
         }
+        if (recommendationInstance == null){
+
+        }
         return recommendationInstance;
     }
 
